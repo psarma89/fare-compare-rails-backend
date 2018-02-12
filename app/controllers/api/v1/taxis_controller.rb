cfca7e905fdb2a6b6637b10f174bc0a5ff5d392b
psarma89 <priyam.sarma@gmail.com>
2018-02-11 21:17:20 -0500

    Adds after updating taxi fare key

--- app/controllers/api/v1/taxis_controller.rb
+++ app/controllers/api/v1/taxis_controller.rb
@@ -1,7 +1,7 @@
 class Api::V1::TaxisController < ApplicationController
 
   def fare
-    response = JSON.parse(RestClient.get("https://api.taxifarefinder.com/fare?key="+ENV['taxi_api_key']+"&origin="+params[:source][:lat]+","+params[:source][:lng]+"&destination="+params[:destination][:lat]+","+params[:destination][:lng]))
+    response = JSON.parse(RestClient.get("https://api.taxifarefinder.com/fare?key="+ENV['taxi_api_key']+"&origin="+params[:source][:lat].to_s+","+params[:source][:lng].to_s+"&destination="+params[:destination][:lat].to_s+","+params[:destination][:lng].to_s))
 
     render json: response
   end
