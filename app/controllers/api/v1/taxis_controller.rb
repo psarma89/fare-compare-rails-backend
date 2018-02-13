f56812576af5b7bf6f59e66dff663c2352494a1b
psarma89 <priyam.sarma@gmail.com>
2018-02-13 13:32:58 -0500

    Adds after adding businesses API for taxi finder

--- app/controllers/api/v1/taxis_controller.rb
+++ app/controllers/api/v1/taxis_controller.rb
@@ -1,7 +1,17 @@
 class Api::V1::TaxisController < ApplicationController
+  skip_before_action :authorized, only: [:fare, :business]
 
   def fare
     response = JSON.parse(RestClient.get("https://api.taxifarefinder.com/fare?key="+ENV['taxi_api_key']+"&origin="+params[:source][:lat].to_s+","+params[:source][:lng].to_s+"&destination="+params[:destination][:lat].to_s+","+params[:destination][:lng].to_s))
+
+    render json: response
+  end
+
+  def business
+    # debugger
+    entity = JSON.parse(RestClient.get("https://api.taxifarefinder.com/entity?key="+ENV['taxi_api_key']+"&location="+params[:source][:lat].to_s+","+params[:source][:lng].to_s))["handle"]
+
+    response = JSON.parse(RestClient.get("https://api.taxifarefinder.com/businesses?key="+ENV['taxi_api_key']+"&entity_handle="+entity))
 
     render json: response
   end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
       resources :users, only: [:create]
       resources :searches, only: [:create, :index]
       post '/taxi_fare', to: 'taxis#fare'
+      post '/businesses', to: 'taxis#business'
       post '/auth', to: 'auth#create'
       get '/current_user', to: 'auth#show'
       patch '/users', to: 'users#update'
